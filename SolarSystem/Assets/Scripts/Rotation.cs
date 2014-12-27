277e86db9719844d14c0cba84450e50352fceca4
dimooon <dimooon.naumenko@gmail.com>
2014-12-27 19:54:20 +0200

    rest of base files

---
+++ SolarSystem/Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotateSpeed = 5.0f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.RotateAround (Vector3.up, rotateSpeed * Time.deltaTime);
+	}
+}
